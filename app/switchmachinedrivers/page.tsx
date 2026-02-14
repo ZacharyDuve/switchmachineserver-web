edd8442a4c31a7688d8e6c0adacba583f228603d
Zachary Duve <zacharyduve@Ganymede.local>
2026-02-13 22:49:50 -0500

    Does it show that I have no idea what I am doing because I dont

--- app/switchmachinedrivers/page.tsx
+++ app/switchmachinedrivers/page.tsx
@@ -7,8 +7,8 @@ const SMDS_HOST_PATH = "/sockets/device";
 export default function SwitchMachineDrivers() {
     const url = `ws://${window.location.host}${SMDS_HOST_PATH}`;
     const [connected, setConnected] = useState(false);
-    const [switchMachineMap, setSwitchMachineMap] = useState<Map<number, SwitchMachineState>>(new Map());
-    let setD = setSwitchMachineMap;
+    const [switchMachines, setSwitchMachines] = useState<Array<SwitchMachine>>([]);
+
     useEffect(() => {
         const ws = new WebSocket(url);
 
@@ -31,10 +31,15 @@ export default function SwitchMachineDrivers() {
                 return;
             }
 
-            //switchMachine = SwitchMachine(switchMachine);
+            // At this point we know that this is a switch machine message lets apply the update.
+            let switchMachineMessage = switchMachine as SwitchMachineMessage
             
-            switchMachineMap.set(switchMachine.id, switchMachine.state);
-            setSwitchMachineMap(switchMachineMap);
+            
+            if (switchMachines.find((sm: SwitchMachine, _: Number): boolean => {
+                return switchMachineMessage.id == sm.id;
+            })) {
+                
+            }
         }
 
         return () => {
@@ -68,15 +73,82 @@ enum SwitchMachineState {
     SettingToB,
 }
 
-interface SwitchMachineMessage {
+class SwitchMachine {
     id: Number
-    state: SwitchMachineState
+    currentState: SwitchMachineState
+    setState: SwitchMachineState
+
+    constructor(id: Number, initialState: SwitchMachineState) {
+        this.id = id;
+        this.currentState = initialState;
+        this.setState = initialState;
+    }
+
+    updateCurrentState(s: SwitchMachineState) {
+        this.currentState = s;
+    }
+
+    updateSetState(s: SwitchMachineState) {
+        this.setState = s;
+    }
+
+    getCurrentState(): SwitchMachineState {
+        return this.currentState;
+    }
+
+    getSetState(): SwitchMachineState {
+        return this.setState
+    }
+
+    equal(other: SwitchMachine): boolean {
+        return this.id == other.id;
+    }
+}
+
+class SwitchMachineList {
+    switchMachines: SwitchMachine[]
+
+    constructor() {
+        this.switchMachines = [];
+    }
+
+    contains(sm: SwitchMachine): boolean {
+        let foundSM = this.switchMachines.find((curSM): boolean => {
+            return curSM.id == sm.id
+        })
+
+        return foundSM != undefined
+    }
+
+    handleSwitchMachineUpdate(id: Number, newState: SwitchMachineState) {
+        let foundSM = this.switchMachines.find((curSM): boolean => {
+            return curSM.id == id
+        })
+
+        if (foundSM) {
+            // We contain so apply update
+            foundSM.currentState = newState;
+            foundSM.setState = newState;
+        } else {
+            // Did not find it is new
+            foundSM = new SwitchMachine(id, newState);
+            this.switchMachines.push(foundSM);
+        }
+    }
+}
+
+interface SwitchMachineMessage {
+    // id: Number
+    // currentState: SwitchMachineState
+    // setState: SwitchMachineState
+    updates: SwitchMachine[]
 }
 
 function isSwitchMachineMessage(a: any): boolean {
     return a &&
         a.id && typeof(a.id) == 'number' &&
-        a.state && Object.values(SwitchMachineState).includes(a.state);
+        a.currentState && Object.values(SwitchMachineState).includes(a.currentState) &&
+        a.setState && Object.values(SwitchMachineState).includes(a.setState);
 }
 
 //function handleWSOnMessage(e: Event, smState: {switchMachines: SwitchMachine, updateSwitchMachines: Dispatch<SetStateAction<never[]>>}) 
